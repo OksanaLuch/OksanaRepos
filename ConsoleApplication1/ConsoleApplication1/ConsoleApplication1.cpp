aa9b6b2427f008d478bc91f881ab75073613b87b
OksanaLuch <luchkina.oksanka@mail.ru>
2021-09-26 18:47:26 +0300

    S pari
    Sozdaly truby, it's svoystva, vively vso.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct Pipe
+{
+	int id;
+	int d;
+	int l; //ot 10 do 200 sdelat' ogranichenie
+};
+
+struct KampStation
+{
+
+};
+
+void PrintePipe(Pipe& p)
+{
+	std::cout << "Pipe number: " << p.id << "It's diameter is: " << p.d << " mm " << " It's lenght: " << p.l << " km ";
+}
+
+Pipe AddPipe()
+{
+	Pipe p;
+	p.id = 0;
+	std::cout << "Vvedite diameter ";
+	std::cin >> p.d;
+	std::cout << "Vvedite lenght ";
+	std::cin >> p.l;
+	return p;
+}
+
+int main()
+{
+	Pipe p = AddPipe();
+	PrintePipe(p);
+}
