f2239939c904ea0c969a2e7d685d6344f6a4c2dc
OksanaLuch <luchkina.oksanka@mail.ru>
2021-09-26 22:44:40 +0300

    everything is ready

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -20,18 +20,22 @@ struct KampStation
 
 void PrintePipe(Pipe& p)
 {
-	cout << "Pipe number: " << p.id << "\n It's diameter is: " << p.d << " mm " << " \n It's lenght: " << p.l << " km "<< "It it's in remont? The answer is "<<p.remont;
+	cout << "Pipe number: " << p.id << "\n It's diameter is: " << p.d << " mm " << " \n It's lenght: " << p.l << " km \n"<< "If it's in remont? The answer is "<<p.remont<<"\n";
 }
 
 Pipe AddPipe()
 {
 	Pipe p;
 	p.id = 1;
-	cout << "Vvedite diameter ";
+	cout << "Vvedite diameter: ";
 	cin >> p.d;
-	cout << "Vvedite lenght ";
+	cout << "Vvedite lenght: ";
 	cin >> p.l;
-	cout << "Vvedite yes, esli truba v remonte. Inache no ";
+	while (p.l <= 10 or p.l >= 200){ 
+		cout << "Length of pipe must be from 10 to 200. Enter it again \n";
+		cin >> p.l;
+		}
+	cout << "Vvedite yes, esli truba v remonte. Inache no: ";
 	cin >> p.remont;
 	return p;
 }
@@ -39,16 +43,16 @@ Pipe AddPipe()
 KampStation AddStation() {
 	KampStation s;
 	s.id = 1;
-	cout << "Vvedite name of station"; cin >> s.name;
-	cout << "Vvedite obshee kolichestvo tsehov"; cin >> s.at;
-	cout << "Vvedite kolichestvo rabitaushih tsehov"; cin >> s.atr;
-	cout << "Vvedite effectivnost' stantsii"; cin >> s.eff;
+	cout << "Vvedite name of station: "; cin >> s.name; 
+	cout << "Vvedite obshee kolichestvo tsehov: "; cin >> s.at;
+	cout << "Vvedite kolichestvo rabitaushih tsehov: "; cin >> s.atr;
+	cout << "Vvedite effectivnost' stantsii: "; cin >> s.eff;
 	return s;
 }
 
 void PrintStation(KampStation& s)
 {
-	cout << "Kompressornaya station with id " << s.id << " is named " << s.name << " It has " << s.at << " tsehov vsego and " << s.atr << " tsehov rabotaut. Effectivnost' of station is " << s.eff;
+	cout << "Kompressornaya station with id " << s.id << " is named " << s.name << " It has " << s.at << " tsehov vsego and " << s.atr << " tsehov rabotaut. Effectivnost' of station is " << s.eff<<"\n";
 }
 
 int main()
@@ -58,26 +62,46 @@ int main()
 	KampStation s;
 
 	do {
-		cout << "1.Add Pipe\n" << "2.Add Kompressornaya station\n" << "3.See all objects\n" << "4.Edit Pipe\n" << "5.Edit Kompressornaya station\n" << "6.Save\n" << "7.Download\n";
+		cout << "1.Add Pipe\n" << "2.Add Kompressornaya station\n" << "3.See all objects\n" << "4.Edit Pipe\n" << "5.Edit Kompressornaya station\n";
+		cin >> b;
 		switch (b)
 		{
 		case 1:
 				p = AddPipe();
-				PrintePipe(p);
 			break;
 		case 2:
 			s=AddStation();
-			PrintStation(s);
 			break;
 		case 3:
-			if (s.id and p.id) { PrintePipe(p); PrintStation(s);}
-			else if (s.id) PrintStation(s);
-			else if (p.id) PrintePipe(p);
-			else cout << "Dannih net. Neobhodimo vvesti.";
+			if (s.id==1) PrintStation(s);
+			if (p.id==1) PrintePipe(p);
+			if (s.id!=1 or p.id!=1) cout << "Dannih net. Neobhodimo vvesti.\n";
 			break;
 		case 4:
+			if (p.id) {
+				int g;
+				cout << "If you want to edit length of pipe enter 1. Else enter 0."; cin >> g;
+				if (g == 1) cin >> p.l;
+				cout << "If you want to edit status of remont of pipe enter 1. Else enter 0."; cin >> g;
+				if (g == 1) cin >> p.remont;
+			}
+			else cout << "Pipe doesn't exist";
+			break;
+		case 5:
+			if (s.id) {
+				int g;
+				cout << "If you want to edit emount of tsehov enter 1. Else enter 0."; cin >> g;
+				if (g == 1) cin >> s.at;
+				cout << "If you want to edit emount of tsehov v rabote enter 1. Else enter 0."; cin >> g;
+				if (g == 1) cin >> s.atr;
+				cout << "If you want to edit effectivmost' of station enter 1. Else enter 0."; cin >> g;
+				if (g == 1) cin >> s.eff;
+			}
+			else cout << "Statin doesn't exist";
+			break;
 		}
-
+		cout << "If you want to ccontinue enter 1. Else enter 0.\n";
+		cin >> a;
 	} 
 	while (a != 0);
 
